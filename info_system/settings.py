4accce0e925b1220f7603623535f40dafb6f6b93
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-03-25 22:10:06 +0800

    更新前端页面
    使用bootstrap更新前端页面样式

--- info_system/settings.py
+++ info_system/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
