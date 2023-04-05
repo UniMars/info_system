37fc9c23091b222c26ad1d75f6f8e96c477095b4
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-04-06 03:22:53 +0800

    重构json_load路径

--- utils/json_load.py
+++ utils/json_load.py
@@ -1,13 +1,15 @@
-# import xlrd
 import csv
 import datetime
 import json
 import os
+import re
 from datetime import date, datetime
 
+import chardet
 import numpy as np
 import pandas as pd
-import chardet
+
+os.system('chcp 65001')
 
 
 class LoadDatetime(json.JSONEncoder):
@@ -34,7 +36,7 @@ class LoadDatetime(json.JSONEncoder):
             return json.JSONEncoder.default(self, obj)
 
 
-def convert_res(res_list: list):
+def convert_std_table(res_list: list):
     """
     将csv文件读取的结果转换为字典
     :param res_list: 一个包含若干个嵌套列表的列表，其中第一个列表包含表头，随后列表包含具体数据
@@ -45,12 +47,20 @@ def convert_res(res_list: list):
     for j in range(len(res_list[1:])):
         d = {}
         for i in range(len(header)):
-            d[header[i].strip()] = res_list[j + 1][i].strip()
+            content = res_list[j + 1][i]
+            # content 类型判断和清洗
+            if isinstance(content, str):
+                content = re.sub('[\u2002\u2003\u3000\xa0]', ' ', content)
+                content = content.strip()
+            try:
+                d[header[i].strip()] = content
+            except Exception as _:
+                print(_)
         res_dict.append(d)
     return res_dict
 
 
-def test(path=r'./DATA/政府网站数据/数据汇总'):
+def json_load(path='.\\DATA\\政府网站数据\\数据汇总'):
     res = {}
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -65,17 +75,19 @@ def test(path=r'./DATA/政府网站数据/数据汇总'):
                         result = list(reader)
                 except Exception as e:
                     print(e)
-                result_dict = convert_res(result)
+                result_dict = convert_std_table(result)
                 res[file.split(".csv")[0]] = result_dict
 
             elif dir_str.endswith(".xlsx") or dir_str.endswith(".xls"):
                 print(dir_str)
                 data = pd.read_excel(dir_str)
                 result = [data.columns.values.tolist()] + np.array(data).tolist()
-                result_dict = convert_res(result)
+                result_dict = convert_std_table(result)
                 res[file.split(".xls")[0]] = result_dict
+    return res
 
 # print("生成json文件中")
 # with open("D:/zl/res_数据汇总.json", "w") as f:
 #     json.dump(res, f, cls=LoadDatetime)
 # print("Done!")
+# test()
