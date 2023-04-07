4bfc378c6dc8d0810882c4e8c0fa72588df39678
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-04-07 18:04:48 +0800

    Update json_load.py
    # 新增
    - generate_id函数
    # 修改
    - load_forms函数添加移动文件

--- utils/json_load.py
+++ utils/json_load.py
@@ -1,8 +1,10 @@
 import csv
 import datetime
+import hashlib
 import json
 import os
 import re
+import shutil
 from datetime import date, datetime
 
 import chardet
@@ -10,28 +12,28 @@ import numpy as np
 import pandas as pd
 
 
-class LoadDatetime(json.JSONEncoder):
-    """
-    重写json的编码器，使其能够将datetime类型的数据转换为字符串
-    """
-
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            try:
-                return obj.strftime('%Y-%m-%d %H:%M:%S')
-            except Exception as _:
-                print(_)
-                print(obj)
-                return None
-        elif isinstance(obj, date):
-            try:
-                return obj.strftime('%Y-%m-%d')
-            except Exception as _:
-                print(_)
-                print(obj)
-                return None
-        else:
-            return json.JSONEncoder.default(self, obj)
+# class LoadDatetime(json.JSONEncoder):
+#     """
+#     重写json的编码器，使其能够将datetime类型的数据转换为字符串
+#     """
+#
+#     def default(self, obj):
+#         if isinstance(obj, datetime):
+#             try:
+#                 return obj.strftime('%Y-%m-%d %H:%M:%S')
+#             except Exception as _:
+#                 print(_)
+#                 print(obj)
+#                 return None
+#         elif isinstance(obj, date):
+#             try:
+#                 return obj.strftime('%Y-%m-%d')
+#             except Exception as _:
+#                 print(_)
+#                 print(obj)
+#                 return None
+#         else:
+#             return json.JSONEncoder.default(self, obj)
 
 
 def convert_std_table(res_list: list):
@@ -58,7 +60,7 @@ def convert_std_table(res_list: list):
     return res_dict
 
 
-def json_load(path='.\\DATA\\政府网站数据\\数据汇总'):
+def load_forms(path: str, if_move=False):
     res = {}
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -82,7 +84,28 @@ def json_load(path='.\\DATA\\政府网站数据\\数据汇总'):
                 result = [data.columns.values.tolist()] + np.array(data).tolist()
                 result_dict = convert_std_table(result)
                 res[file.split(".xls")[0]] = result_dict
+
+            # move file to move_path
+            if if_move:
+                move_path = os.path.join(root, '/processed')
+                if not os.path.exists(move_path):
+                    os.makedirs(move_path)
+                shutil.move(os.path.join(root, file), move_path)
     return res
+
+
+def generate_id(word: str, record_id: int):
+    # 使用哈希函数（如SHA-256）计算word的哈希值
+    hash_obj = hashlib.sha256(word.encode())
+    hash_value = int(hash_obj.hexdigest(), 16)
+
+    # 取哈希值的低32位
+    hash_lower_32_bits = hash_value & 0xFFFFFFFF
+
+    # 将哈希值与record_id组合以生成64位ID
+    id_64_bits = (hash_lower_32_bits << 32) | record_id
+
+    return id_64_bits
 
 # print("生成json文件中")
 # with open("D:/zl/res_数据汇总.json", "w") as f:
