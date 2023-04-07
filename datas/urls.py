462b12f42e6fc9ccabea758a7f21621ddacfe83f
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-04-07 18:02:54 +0800

    模型升级

--- datas/urls.py
+++ datas/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('import', views.data_import, name='import'),
+    path('import', views.gov_data_import, name='import'),
     path('updateTable', views.table_update, name='updateTable'),
     path('wordSplit', views.word_split, name='wordSplit'),
     path('wordCloud', views.wordcloud, name="wordcloud")
--- report/admin.py
+++ report/admin.py
@@ -1,6 +1,5 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Question
 
-admin.site.register(Question)
+# admin.site.register(Question)
