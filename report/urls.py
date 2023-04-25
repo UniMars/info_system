9530f3d261ab170099f5c9c07ab69d071e3a2cb6
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-04-26 01:06:14 +0800

    feat(app report): add new URL

--- report/urls.py
+++ report/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('graph/', views.graph_index, name='graph'),
+    path('graph/generate_graph', views.generate_graph, name='generate_graph')
 ]
