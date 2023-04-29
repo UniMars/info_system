76ee0908a3f3d0ad7789c1590ba17d78dcf9eb9d
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-04-29 23:48:43 +0800

    workflow: 更新config.bat添加celery 运行指令

--- config.bat
+++ config.bat
@@ -1,3 +1,6 @@
 pipreqs . --encoding=utf-8 --force
 python manage.py makemigrations
 python manage.py migrate
+
+celery -A info_system worker --loglevel=debug --logfile="./logs/celery/worker.log" -P gevent
+celery -A info_system beat --loglevel=debug --logfile="./logs/celery/beat.log"
