11551454a54053ef084f5334acb219ea5ffed3ae
马明宇(MA MINGYU) <ares.mmy@qq.com>
2023-05-03 22:56:50 +0800

    workflow: 添加celery启动命令

--- config.bat
+++ config.bat
@@ -1,6 +1,9 @@
 pipreqs . --encoding=utf-8 --force
 python manage.py makemigrations
 python manage.py migrate
-
+net stop celery_worker
+net stop celery_beat
+net start celery_worker
+net start celery_beat
 celery -A info_system worker --loglevel=debug --logfile="./logs/celery/worker.log" -P gevent
 celery -A info_system beat --loglevel=debug --logfile="./logs/celery/beat.log"
